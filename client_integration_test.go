fee01c4d87903a961ce6567413661af813fbc93e
Adam Shannon <adamkshannon@gmail.com>
2025-03-13 10:12:04 -0500

    test: update vsftpd integration test to use docker-compose

--- client_integration_test.go
+++ client_integration_test.go
@@ -1,75 +1,36 @@
-///////go:build integration
-
-package go_ftp
+package go_ftp_test
 
 import (
 	"bytes"
-	"context"
-	"fmt"
-	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 	"io"
 	"testing"
-	"time"
+
+	go_ftp "github.com/moov-io/go-ftp"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestIntegrationUploadFile(t *testing.T) {
-	t.Run("UploadFile with CreateUploadDirectories=true should create directories", func(t *testing.T) {
-		ftpHost, ftpTerm := setupFTPServer(t)
-		defer ftpTerm()
+func TestIntegration_fauria_vsftpd(t *testing.T) {
+	config := go_ftp.ClientConfig{
+		Hostname: "localhost:4021",
+		Username: "ftpuser",
+		Password: "ftppass",
 
-		// Create a new FTP client
-		client, err := NewClient(ClientConfig{
-			Hostname: ftpHost,
-			Username: "ftpuser",
-			Password: "ftppass",
-			Timeout:  10 * time.Second,
-			//
-			CreateUploadDirectories: true,
-			//
-			DisableEPSV: false,
-			CAFile:      "",
-			TLSConfig:   nil,
-		})
-		require.NoError(t, err)
-
-		// Upload a file
-		err = client.UploadFile("2025/04/03/test.txt", io.NopCloser(bytes.NewReader([]byte("hello world"))))
-		require.NoError(t, err)
-	})
-}
-
-func setupFTPServer(t *testing.T) (string, func()) {
-	t.Helper()
-
-	ctx := context.TODO()
-
-	req := testcontainers.ContainerRequest{
-		Image:        "fauria/vsftpd:latest",
-		ExposedPorts: []string{"20/tcp", "21/tcp", "21100-21110/tcp"},
-		WaitingFor:   wait.ForListeningPort("21/tcp"),
-		Env: map[string]string{
-			"FTP_USER": "ftpuser",
-			"FTP_PASS": "ftppass",
-		},
+		CreateUploadDirectories: true,
 	}
+	client, err := go_ftp.NewClient(config)
+	require.NoError(t, err)
+	require.NotNil(t, client)
 
-	ftpContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
+	t.Run("UploadFile with CreateUploadDirectories", func(t *testing.T) {
+		err := client.UploadFile("2025/test.txt", io.NopCloser(bytes.NewReader([]byte("hello world"))))
+		require.NoError(t, err)
+
+		file, err := client.Open("2025/04/03/test.txt")
+		require.NoError(t, err)
+
+		bs, err := io.ReadAll(file)
+		require.NoError(t, err)
+		require.Equal(t, "hello world", string(bs))
 	})
-	require.NoError(t, err)
-
-	// Get the host and port of the running container
-	host, err := ftpContainer.Host(ctx)
-	require.NoError(t, err)
-
-	port, err := ftpContainer.MappedPort(ctx, "21")
-	require.NoError(t, err)
-
-	ftpHost := fmt.Sprintf("%s:%s", host, port.Port())
-	ftpTerm := func() { ftpContainer.Terminate(ctx) }
-
-	return ftpHost, ftpTerm
 }
